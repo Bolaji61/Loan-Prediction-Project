3da4940260f21fd53b8f5e7823c9da93d601d77f
Bolaji61 <balogunbolaji61@gmail.com>
2020-06-14 05:10:17 +0100

    Creating an endpoint for the model and a web app

---
+++ app.py
@@ -0,0 +1,36 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import joblib
+
+app = Flask(__name__)
+model =joblib.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [i for i in request.form.values()]
+    int_features = np.array(int_features).astype(np.float64)
+    final_features = [np.array(int_features)]
+    print(int_features)
+    prediction = model.predict(final_features)
+
+    # output = round(prediction[0], 2)
+    output = prediction[0]
+    
+
+    return render_template('index.html', prediction_text='Loan Status is {}'.format(output))
+
+@app.route('/results',methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
